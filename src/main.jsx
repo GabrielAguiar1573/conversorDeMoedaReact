7f89d8e2e36fa6dcafc0931cf9fb9cdad6225b43
GabrielAguiar1573 <gabrielhenriquedeaguiar@gmail.com>
2024-05-18 18:19:34 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ConversorDeMoedas from './ConversorDeMoedas.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ConversorDeMoedas />
+  </React.StrictMode>,
+)
